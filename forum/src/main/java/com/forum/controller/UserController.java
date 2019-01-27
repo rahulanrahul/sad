c276e96f43e0f16d797ca47a6e90d0d6b4abec46
Rahul Sarode <rahulsarode9@gmail.com>
2019-01-27 15:51:53 +0100

    Revert "Change URI's of User controller to make it RESTful"
    This reverts commit 42b9f85479dbdd0ff2f035f236c5ca4b728d3fbb.

--- forum/src/main/java/com/forum/controller/UserController.java
+++ forum/src/main/java/com/forum/controller/UserController.java
@@ -5,44 +5,43 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.forum.model.UserDetailsModel;
 import com.forum.service.UserService;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/user")
 public class UserController {
 	@Autowired
 	UserService userService;
 
 	@CrossOrigin(origins = "http://localhost:4200")
-	@PostMapping("/users")
+	@PostMapping("/post-userdetails")
 	public ResponseEntity<String> postuserDetails(@RequestBody UserDetailsModel userModel) {
 		return userService.postUser(userModel);
 	}
 
 	@CrossOrigin(origins = "http://localhost:4200")
-	@GetMapping("/users/{userId}")
-	public ResponseEntity<UserDetailsModel> getuserDetails(@PathVariable int userId) {
+	@GetMapping("/get-userdetails")
+	public ResponseEntity<UserDetailsModel> getuserDetails(@RequestParam(required = false) int userId) {
 		return userService.getUser(userId);
 	}
 
 	@CrossOrigin(origins = "http://localhost:4200")
-	@PutMapping("/users/{userId}")
-	public ResponseEntity<String> edituserDetails(@PathVariable int userId, @RequestBody UserDetailsModel userModel) {
-		userModel.setUserId(userId);
+	@PutMapping("/edit-userdetails")
+	public ResponseEntity<String> edituserDetails(@RequestBody UserDetailsModel userModel) {
 		return userService.editUser(userModel);
 	}
 
 	@CrossOrigin(origins = "http://localhost:4200")
-	@DeleteMapping("/users/{userId}")
-	public ResponseEntity<String> deleteuserDetails(@PathVariable int userId) {
+	@DeleteMapping("/delete")
+	public ResponseEntity<String> deleteuserDetails(@RequestParam(required = false) int userId) {
 		return userService.deleteUser(userId);
 	}
 	
